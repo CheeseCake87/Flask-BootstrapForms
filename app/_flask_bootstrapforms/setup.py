62d22a19eb8808deca500bcffcc116178452d68c
David Carmichael <carmichaelits@gmail.com>
2022-07-23 12:52:59 +0100

    version 0.1.6 released

--- app/_flask_bootstrapforms/setup.py
+++ app/_flask_bootstrapforms/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='Flask-BootstrapForms',
-    version='0.1.5',
+    version='0.1.6',
     url='https://github.com/CheeseCake87/Flask-BootstrapForms',
     license='MIT',
     author='David Carmichael',
